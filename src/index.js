c02c195fc54e6add6560b283be729acadc8ebc70
Paula <psanzilundain@gmail.com>
2024-04-19 13:29:51 +0200

    swagger.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
+const swaggerUI = require('swagger-ui-express');
 
 require('dotenv').config();
 
