fe94afd8ec02dd18d28daa7097b15a84193fb58b
Paula <psanzilundain@gmail.com>
2024-04-19 11:31:57 +0200

    endpoints put and delete working right

--- src/index.js
+++ src/index.js
@@ -63,3 +63,80 @@ server.post('/newwork', async (req, res) => {
     message: 'Todo correcto',
   });
 });
+
+server.put('/works/:id_work', async (req, res) => {
+  const idWork = req.params.id;
+
+  const newData = req.body;
+  const { title, year, image } = newData;
+
+  const connection = await getDBConnection();
+  const updateQuery = `UPDATE work SET title= ?, year = ?, image= ? WHERE id_work = ?`;
+  const [result] = await connection.query(updateQuery, [
+    title,
+    year,
+    image,
+    idWork,
+  ]);
+  res.status(200).json({
+    success: true,
+  });
+});
+server.put('/artists/:id', async (req, res) => {
+  const idArtist = req.params.id;
+
+  const newData = req.body;
+  const { name, lastname, movement, picture } = newData;
+
+  const connection = await getDBConnection();
+  const updateQuery = `UPDATE artists SET name= ?, lastname = ?, movement= ?, picture= ? WHERE id= ?`;
+  const [result] = await connection.query(updateQuery, [
+    name,
+    lastname,
+    movement,
+    picture,
+    idArtist,
+  ]);
+  res.status(200).json({
+    success: true,
+  });
+});
+
+server.delete('/works/:id_work', async (req, res) => {
+  const idWork = req.params.id_work;
+
+  const connection = await getDBConnection();
+
+  const deleteQuery = `DELETE FROM work WHERE id_work = ?`;
+  const [result] = await connection.query(deleteQuery, [idWork]);
+  if (result.affectedRows > 0) {
+    res.status(200).json({
+      success: true,
+      message: 'Elemento eliminado',
+    });
+  } else {
+    res.status(400).json({
+      success: false,
+      message: 'No se ha eliminado el elemento',
+    });
+  }
+});
+server.delete('/artists/:id', async (req, res) => {
+  const idArtist = req.params.id;
+
+  const connection = await getDBConnection();
+
+  const deleteQuery = `DELETE FROM artists WHERE id = ?`;
+  const [result] = await connection.query(deleteQuery, [idArtist]);
+  if (result.affectedRows > 0) {
+    res.status(200).json({
+      success: true,
+      message: 'Elemento eliminado',
+    });
+  } else {
+    res.status(400).json({
+      success: false,
+      message: 'No se ha eliminado el elemento',
+    });
+  }
+});
